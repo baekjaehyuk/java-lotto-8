2e260707b6f75edaabd7b836841be22522abc657
baekjaehyuk <bjh010321@naver.com>
2025-11-03 14:30:58 +0900

    feat: 로또 관련 상수 관리

---
+++ src/main/java/lotto/util/LottoConstants.java
@@ -0,0 +1,14 @@
+package lotto.util;
+
+import java.math.BigDecimal;
+
+public class LottoConstants {
+
+    public static final int LOTTO_NUMBER_COUNT = 6;
+    public static final int MIN_LOTTO_NUMBER = 1;
+    public static final int MAX_LOTTO_NUMBER = 45;
+    public static final BigDecimal PRICE_PER_LOTTO = BigDecimal.valueOf(1000);
+
+    private LottoConstants() {
+    }
+}
